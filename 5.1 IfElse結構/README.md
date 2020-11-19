3ccd933834e466eae2c2e86f26cacdf9bb83ef61
ifalo-ken <ken@ifalo.com>
2020-11-19 16:01:31 +0800

    理解if else 與其他語言的差異

---
+++ 5.1 IfElse結構/README.md
@@ -0,0 +1,20 @@
+# if else 結構控制
+> 基本的我就跳過了 我只講與php的差異  
+> 註： 筆者本身從phｐ開始學習 
+
+
+### 沒有括弧
+> 在if switch for 當中 是不使用括弧的 好處是看起來非常簡潔 壞處: 需要一段時間才能習慣
+
+
+### 塞入變數
+> 在if switch for 迴圈當中 可以適度的塞入變數 並進行判斷 
+> 但只能在該迴圈當中做使用
+
+```go
+if v:= true; v == true {   // 也可以塞入function 回傳的值
+    //do something
+} else{
+    // do something
+}
+```
---
+++ 5.1 IfElse結構/string_conversion2.go
@@ -0,0 +1,27 @@
+package main
+
+// 改寫 err 放入if 當中
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var orig string = "ABC"
+	// var an int
+	var newS string
+	// var err error
+
+	fmt.Printf("The size of ints is: %d\n", strconv.IntSize)
+	// anInt, err = strconv.Atoi(origStr)
+
+	if an, err := strconv.Atoi(orig); err != nil {
+		fmt.Printf("orig %s is not an integer - exiting with error\n", orig)
+		return
+	} else {
+		fmt.Printf("The integer is %d\n", an)
+		an = an + 5
+		newS = strconv.Itoa(an)
+	}
+	fmt.Printf("The new string is: %s\n", newS)
+}
